38446a4670e1a50d06d35a836db62350afbe49ec
Alejandro <alejo8a_2@hotmail.com>
2016-04-22 07:34:29 -0500

    Listo la creacion de matrices

--- Practica2/Main.cpp
+++ Practica2/Main.cpp
@@ -1,7 +1,15 @@
 //guia https://msdn.microsoft.com/en-us/library/4ks26t93.aspx
+//guia2 http://www.jegerlehner.ch/intel/IntelCodeTable_es.pdf no sirve para floats
 
 #include <iostream>
 using namespace std;
+
+float pSuma(float a, float b);
+float pResta(float a, float b);
+float pMultiplicacion(float a, float b);
+float pDivision(float a, float b);
+
+float** crearMatriz();
 
 float pSuma(float a, float b) {
 	float res;
@@ -49,10 +57,34 @@ float pResta(float a,float b) {
 
 }
 
+float** crearMatriz() {
+	const int tam = 4;
+	
+	float** matrix = 0;
+	matrix = new float*[tam];
+	for (size_t i = 0; i < tam; i++) {
+		matrix[i] = new float[tam];
+		cout << "Fila " << i << " ";
+		for (size_t j = 0; j < tam; j++) {
+			cout << "Columna " << j  << endl;
+			cin >> matrix[i][j];
+		}
+	}
+	return matrix;
+}
+
 
 int main() {
-	double res = pDivision(1.4, 0.5);
-	cout << res << endl;
+	const int n = 4;
+	float** a = crearMatriz();
+	
+	for (size_t i = 0; i < n; i++){
+		for (size_t j = 0; j < n; j++) {
+			cout << a[i][j] << ", ";
+		}
+		cout << endl;
+	}
+
 	system("pause");
 	return 0;
 }
